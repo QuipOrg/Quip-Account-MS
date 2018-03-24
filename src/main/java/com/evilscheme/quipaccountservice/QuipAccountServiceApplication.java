65a789044bdf121a50301951cc53d1f4067412d3
Matheu DeWitt <msdewitt@bsu.edu>
2018-03-24 16:07:00 -0400

    Project init.

---
+++ src/main/java/com/evilscheme/quipaccountservice/QuipAccountServiceApplication.java
@@ -0,0 +1,12 @@
+package com.evilscheme.quipaccountservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuipAccountServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuipAccountServiceApplication.class, args);
+	}
+}
